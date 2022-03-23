ead1634949cc655b1389682e176abaa2cf76eb26
Brice Roehrig <broehrig.contact@gmail.com>
2022-03-23 22:40:16 +0100

    new form and validation,bug on title blank

--- app/models/ski.rb
+++ app/models/ski.rb
@@ -1,9 +1,10 @@
 class Ski < ApplicationRecord
   belongs_to :user
-  validates :brand, :description, :price, :sizing, presence: true
-  validates :category, presence: true
+  validates :brand, presence: true, inclusion: { in: %w[Rossignol Salomon Atomic K2 Volkl Dynastar Smith Marker Scott Black\ Crows Oakley Faction Bjorn Daehlie Cairn Armada Bolle Swix Blizzard Head Vola]}
+  validates :title, :description, :price, presence: true
 end
 
 # message: "%{value} is not a valid category"
 
+# validates :brand, :description, :price, :sizing, presence: true
 # validates :category, presence: true, inclusion: { in: %w[Ski\ Alpin Ski\ de\ Rando Ski\ de\ Fond Snowboard]}
