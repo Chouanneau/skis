6288262b8d74e4ac7ffffe4b2c0de0b191ab6915
Matthieu Chouanneau <m.chouanneau@gmail.com>
2022-03-23 15:20:11 +0100

    message erreur

--- app/models/ski.rb
+++ app/models/ski.rb
@@ -1,6 +1,7 @@
 class Ski < ApplicationRecord
   belongs_to :user
   validates :brand, :description, :price, :sizing, presence: true
-  validates :category, presence: true, inclusion: { in: %w[Ski\ Alpin Ski\ de\ Rando Ski\ de\ Fond Snowboard]
-                                                    message: "%{value} is not a valid category" }
+  validates :category, presence: true, inclusion: { in: %w[Ski\ Alpin Ski\ de\ Rando Ski\ de\ Fond Snowboard]}
 end
+
+# message: "%{value} is not a valid category"
