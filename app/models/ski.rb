06e5da9b5392d2f8eb84fec2a8de1ce6d92952ab
Brice Roehrig <broehrig.contact@gmail.com>
2022-03-23 22:17:17 +0100

    seed works but error when runnung db:seed

--- app/models/ski.rb
+++ app/models/ski.rb
@@ -1,7 +1,9 @@
 class Ski < ApplicationRecord
   belongs_to :user
   validates :brand, :description, :price, :sizing, presence: true
-  validates :category, presence: true, inclusion: { in: %w[Ski\ Alpin Ski\ de\ Rando Ski\ de\ Fond Snowboard]}
+  validates :category, presence: true
 end
 
 # message: "%{value} is not a valid category"
+
+# validates :category, presence: true, inclusion: { in: %w[Ski\ Alpin Ski\ de\ Rando Ski\ de\ Fond Snowboard]}
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "nokogiri"
+require "open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,8 +7,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# user1 = User.create!(username: "Matthieu", address: "danstoncul", email: "test@gmail.com", password: "1213456")
-Ski.destroy_all
 
-ski1 = Ski.create!(brand: "Rossignol", category:"alpin", sizing:"180", description: "Superbes !", price: 34, user: user1)
-ski2 = Ski.create!(brand: "Salomon", category:"alpin", sizing:"134", description: "Magiques !", price: 289, user: user1)
+
+
+# idee de scraping pour les skis
+
+Ski.destroy_all
+User.destroy_all
+
+# user1 = User.create!(username: "Matthieu", address: "danstoncul", email: "test@gmail.com", password: "1213456")
+# ski1 = Ski.create!(brand: "Rossignol", category:"alpin", sizing:"180", description: "Superbes !", price: 34, user: user1)
+# ski2 = Ski.create!(brand: "Salomon", category:"alpin", sizing:"134", description: "Magiques !", price: 289, user: user1)
+# file = "skis.html"
+# idee de scraping pour les skis
+
+# Ski.destroy_all
+
+file = "skis.html"
+doc = Nokogiri::HTML(File.open(file), nil, "utf-8")
+user = User.create!(username: "demo", email: "demo@gmail.com", password: "123456", address: "Chemin des Joncquilles, Renens")
+
+
+# # # user2 = User.create!(username: "2", email: "2@gmail.com", password: "123456", address: "Chemin des Joncquilles, Renens")
+# # user5 = User.create!(username: "5", email: "5@gmail.com", password: "123456", address: "Chemin des Joncquilles, Renens")
+
+
+doc.search('.df-card').each do |element|
+  title = element.search('.df-card__title').text.strip
+  brand = element.search('.df-card__brand').text.strip
+  description = element.search('.df-card__description').text.strip
+  sizing = element.search('.d-inline-block').text.strip
+  price = element.search('.df-card__price').text.strip
+  category = "Ski Aplin"
+  # photo_url = element.search('.df-card__image').text.strip
+  # price = ...
+  # photo_url = ...
+  # photo_file = URI.open(photo_url)
+
+  ski = Ski.new(title: title, brand: brand, description: description, sizing: sizing, price: price, category: category, user: user)
+  # ski.attach.photo(io: photo_file)
+  ski.save!
+end
