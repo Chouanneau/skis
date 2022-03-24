ae5025511bbf2dd4e2e82ad13e50016ed3e8a075
Matthieu Chouanneau <m.chouanneau@gmail.com>
2022-03-24 11:01:37 +0100

    search bar ok

--- app/models/ski.rb
+++ app/models/ski.rb
@@ -2,6 +2,12 @@ class Ski < ApplicationRecord
   belongs_to :user
   validates :brand, :description, :price, :sizing, presence: true
   validates :category, presence: true, inclusion: { in: %w[Ski\ Alpin Ski\ de\ Rando Ski\ de\ Fond Snowboard]}
-end
 
-# message: "%{value} is not a valid category"
+  include PgSearch::Model
+  pg_search_scope :search_by_brand_and_description,
+    against: [ :brand, :description ],
+    using: {
+      tsearch: { prefix: true }
+    }
+
+end
